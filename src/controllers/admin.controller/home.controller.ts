bee82b876baea2ef78d141e1b6f92e4dbf832de8
unknown <phungdacnam1997@gmail.com>
2023-06-27 14:12:10 +0700

    1

--- src/controllers/admin.controller/home.controller.ts
+++
@@ -1,9 +0,0 @@
-import AuthController from "../auth.controller";
-
-export class homeController {
-    static async getHomePage(req: any, res: any) {
-        let admin = await AuthController.getInfoUser(req, res)
-        res.render('admin/home', { admin });
-    }
-
-}
--- src/models/data-source.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export class MongoDB {
-    static async connectDB() {
-        const DB_URL = 'mongodb://127.0.0.1:27017/restaurant_manager';
-        return await mongoose.connect(DB_URL);
-    }
-}
