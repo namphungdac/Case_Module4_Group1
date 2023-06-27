4e45915b3c9d4159eb075b89455d615908113a02
KY <nguyenhoangky23121997@gmail.com>
2023-06-27 08:45:34 +0700

    dump home

--- src/controllers/admin.controller/home.controller.ts
+++ src/controllers/admin.controller/home.controller.ts
@@ -2,10 +2,9 @@ import User from "../../models/schemas/user.schema";
 import AuthController from "../auth.controller";
 
 export class homeController {
-
     static async getHomePage(req: any, res: any) {
         let admin = await AuthController.getInfoUser(req, res)
-        res.render('admin/home', {admin});
+        res.render('admin/home', { admin });
     }
 
 }
--- src/controllers/general.controller.ts
+++ src/controllers/general.controller.ts
@@ -1,7 +1,19 @@
-
+import User from "../models/schemas/user.schema";
+import Food from "../models/schemas/food.schema";
+import FoodType from "../models/schemas/foodType.schema";
 export class GeneralController {
-    static getHomePage(req: any, res: any) {
-        res.render('home');
+    static  async getHomePage(req: any, res: any) {
+        const foodTypeGril = await FoodType.findOne({ name: "Món Nướng" })
+        const foodTypeChay = await FoodType.findOne({ name: "Món Chay" })
+        const foodTypeEu = await FoodType.findOne({ name: "Món Âu" })
+        const foodTypeJav = await FoodType.findOne({ name: "Món Nhật" })
+        const foodTypeDrink = await FoodType.findOne({ name: "Đồ Uống" })
+        const grilFood = await Food.find({ type: foodTypeGril._id }).limit(4)
+        const chayFood = await Food.find({ type: foodTypeChay._id }).limit(4)
+        const euFood = await Food.find({ type: foodTypeEu._id }).limit(4)
+        const javFood = await Food.find({ type: foodTypeJav._id }).limit(4)
+        const drinkFood = await Food.find({ type: foodTypeDrink._id }).limit(4)
+        res.render('home', { grils: grilFood, chays: chayFood, eus: euFood, javs: javFood, drinks: drinkFood })
     };
     static getNotFoundPage(req: any, res: any) {
         res.render('notFound');
@@ -10,4 +22,5 @@ export class GeneralController {
     static getMenuPage(req: any, res: any) {
         res.render('menuFood')
     }
+    
 }
