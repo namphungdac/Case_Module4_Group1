487dcfa5a94925bfb7236106f8962e9c75bd100d
Hyboy95 <temilator@gmail.com>
2023-06-27 12:21:23 +0700

    order_admin

--- src/controllers/customer.controller/order.controller.ts
+++ src/controllers/customer.controller/order.controller.ts
@@ -1,10 +1,42 @@
 import Table from "../../models/schemas/table.schema";
+import Order from "../../models/schemas/order.schema";
 import AuthController from "../auth.controller"
 
 export class OrderController {
     static async getOrderPage(req: any, res: any) {
-        let customer = await AuthController.getInfoUser(req, res);
-        let tables = await Table.find({status: 'Trống'});
-        res.render('customer/order', {customer, tables})
+        try {
+            let customer = await AuthController.getInfoUser(req, res);
+            let tables = await Table.find();
+            res.render('customer/order', {customer, tables})
+        } catch (err) {
+            console.log(err.messages);
+        }
+
+    }
+    static async orderTable(req: any, res: any) {
+        try {
+            let {customerName, phone, checkin, numberPerson, other} = req.body;
+            
+            let user = await AuthController.getInfoUser(req, res);
+            let newOrder = new Order({
+                user,
+                phone,
+                numberPerson,
+                timeOrder: Date.now(),
+                table: null,
+                checkin,
+                customerName,
+                other
+            })
+            
+            if (newOrder) {
+                await newOrder.save();
+                res.redirect('/customer/order')
+            } else {
+                res.render('notFound');
+            }
+        } catch (err) {
+            console.log(err.messages);
+        }
     }
 }
